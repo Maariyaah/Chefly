78195aaee7affced7b249c5396867c1cf13b8183
Beatriz Vieira <beaferreirav@gmail.com>
2023-08-23 12:57:48 +0100

    show page and index page html, bootstrap and css

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+
+  static targets = ["words"];
   connect() {
     this.element.textContent = "Hello World!"
   }
+
+
 }
