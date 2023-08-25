f2c63e481b7545e33db12f1b45e2888a8336ac72
Beatriz Vieira <beaferreirav@gmail.com>
2023-08-25 14:30:19 +0100

    controller destroy bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,33 @@
 class BookingsController < ApplicationController
   def index
    @bookings = Booking.all
-  @booking.chef_offer = ChefOffer.find(params["chef_offer_id"])
+
   end
 
   def create
-
+    @chef_offer = ChefOffer.find(params[:chef_offer_id])
     @booking = Booking.new(bookings_params)
-
-    # @booking.current_user = @booking
-    # raise
-
-    #   if @booking.save
-    #     redirect_to profile_path
-    #   else
-    #     render :new, status: :unprocessable_entity
-    #   end
+    @booking.chef_offer = @chef_offer
+    @booking.user = current_user
+    if @booking.save
+      redirect_to profile_path
+    else
+      flash[:alert] = "Date can't be blank!"
+      render "chef_offers/show"
+    end
+    
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+
+  end
+
+
+private
+
   def bookings_params
-    params.require(:Booking).permit(:starting)
+    params.require(:booking).permit(:starting)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :chef_offer
+
+  validates :starting, presence: true
 end
